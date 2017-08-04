60344462d761f927f204b35bd9dba20345d333b0
nixdasmarinas <mddasmarinas@up.edu.ph>
2017-08-04 11:48:34 +0800

    Zipcode added

--- GitPractice0804/src/com/forrest/Git0804.java
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -7,10 +7,12 @@ public class Git0804 {
 		String myName = "Nix";
 		String myEmail = "mddasmarinas@gmail.com";
 		String myAddress = "Caloocan,Philippines";
+		int zipcode = 456;
 		int  myNum = 0324;
 		System.out.println(myName);
 		System.out.println(myEmail);
 		System.out.println(myAddress);
+		System.out.println(zipcode);
 		System.out.println(myNum);		
 	}
 
