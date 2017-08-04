964af5b3d941f3717afbbdf7e4dd079b19089fbb
nixdasmarinas <mddasmarinas@up.edu.ph>
2017-08-04 11:58:33 +0800

    Age commit

--- GitPractice0804/src/com/forrest/Git0804.java
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -8,11 +8,12 @@ public class Git0804 {
 		String myEmail = "mddasmarinas@gmail.com";
 		String myAddress = "Caloocan,Philippines";
 		int  myNum = 0324;
+		int age = 22;
 		System.out.println(myName);
 		System.out.println(myEmail);
 		System.out.println(myAddress);
 		System.out.println(myNum);		
-		
+		System.out.println(age);	
 		for(int i=0; i<5; i++) {
 			
 			System.out.println(i);
