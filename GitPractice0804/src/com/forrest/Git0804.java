66fc8e932697fe602e9f6e80aad5050312d08fb9
nixdasmarinas <mddasmarinas@up.edu.ph>
2017-08-04 11:27:31 +0800

    Sample commit

---
+++ GitPractice0804/src/com/forrest/Git0804.java
@@ -0,0 +1,16 @@
+package com.forrest;
+
+public class Git0804 {
+
+	public static void main(String[] args) {
+		
+		String myName = "Nix";
+		String myEmail = "mddasmarinas@gmail.com";
+		String myAddress = "Caloocan,Philippines";
+		System.out.println(myName);
+		System.out.println(myEmail);
+		System.out.println(myAddress);
+		
+	}
+
+}
